cacec86cf3730bd51acc6b8b568103e8a4d505a9
jeongho7yeo <jeongho7.yeo@gmail.com>
2023-04-13 16:23:23 +0900

    add key template

---
+++ stats/hamicelo/config.js
@@ -0,0 +1,17 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "YOU API KEY",
+    authDomain: "AUTH DOMAIN",
+    projectId: "PROJECTI ID",
+    storageBucket: "STORAGE BUCKET",
+    messagingSenderId: "SENDER ID",
+    appId: "APP ID",
+    measurementId: "MEASURE ID"
+};
+
+const googleConfig = {
+    apiKey: 'GOOGLE API KEY',
+    sheetId_data: 'GOOGLE SHEET ID',
+    driveFolerId: 'DRIVE FOLDER ID',
+}
---
+++ stats/kselo/config.js
@@ -0,0 +1,18 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "YOU API KEY",
+    authDomain: "AUTH DOMAIN",
+    projectId: "PROJECTI ID",
+    storageBucket: "STORAGE BUCKET",
+    messagingSenderId: "SENDER ID",
+    appId: "APP ID",
+    measurementId: "MEASURE ID"
+};
+
+const googleConfig = {
+    apiKey: 'GOOGLE API KEY',
+    sheetId_data: 'GOOGLE SHEET ID',
+    youTubeChannelId: 'YOUTUBE CHANNEL ID',
+    youTubeKey: 'YOUTUBE API KEY',
+}
